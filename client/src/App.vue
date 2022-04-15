e9eb0af5875b4b5c511729d58fa76ba94d74f4e1
kukwa clovis <kukwaclovisngong3@gmail.com>
2022-04-15 17:03:14 +0100

    major commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
-``
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -13,10 +9,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
 }
 
 nav a {
