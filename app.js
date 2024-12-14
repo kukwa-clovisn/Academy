c0f370372e6d0edcf3132fb696754c2aab25ebe5
kukwa clovis <kukwaclovisngong3@gmail.com>
2024-12-14 15:40:19 +0100

    website version two

--- app.js
+++ app.js
@@ -52,51 +52,27 @@ app.use(
 //  Route paths
 const signupRoute = require("./routes/signup");
 const signinRoute = require("./routes/signin");
-const userRoute = require("./routes/user");
 const tokenRoute = require("./routes/token");
-const adminRoute = require("./routes/admin");
-const postRoute = require("./routes/post");
-const todoRoute = require("./routes/todo");
 const forget_passwordRoute = require("./routes/forget_password");
-const registerRoute = require("./routes/register");
-const chatgptRoute = require("./routes/chatGPT");
 
 // getting Routes
 app.use("/api/signup", signupRoute);
 app.use("/api/signin", signinRoute);
-app.use("/api/user", userRoute);
 app.use("/api/token", tokenRoute);
-app.use("/api/admin", adminRoute);
-app.use("/api/post", postRoute);
-app.use("/api/todo", todoRoute);
 app.use("/api/forget_password", forget_passwordRoute);
-app.use("/api/register", registerRoute);
-app.use("/api/chatgpt", chatgptRoute);
 
-/**
- * for app production
- */
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(__dirname, "/public/"));
-
-//   // handling client page routing
-//   app.get(/.*/, (req, res) => {
-//     res.sendFile(__dirname, "/public/index.html");
-//   });
-// }
-
-mongoose
-  .connect(mongo_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((res) => {
-    console.log("Database connected");
-    app.listen(port, () => {
-      console.log(
-        `Advanced Tech Academy server up and running on http://localhost:${port}...`
-      );
-    });
-  })
-  .catch((err) => console.log(err));
+app.listen(port, () => console.log(port));
+// mongoose
+//   .connect(mongo_uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((res) => {
+//     console.log("Database connected");
+//     app.listen(port, () => {
+//       console.log(
+//         `Advanced Tech Academy server up and running on http://localhost:${port}...`
+//       );
+//     });
+//   })
+//   .catch((err) => console.log(err));
